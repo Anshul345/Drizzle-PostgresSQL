aee033541f586fb8832f2f3f1d46b584ef0bd394
Anshul345 <yadavanshul05493@gmail.com>
2026-01-15 17:55:42 +0530

    Secon Comment

--- Drizzle-postgres/app.js
+++ Drizzle-postgres/app.js
@@ -1,13 +1,21 @@
 import { db } from "./src/db.js";
 import { users } from "./src/schema.js";
-import {  eq,
+import {
+  eq,
   and,
   or,
   like,
   inArray,
   between,
   desc,
-  asc, } from 'drizzle-orm';
+  asc,
+  count,
+  sum,
+  avg,
+  min,
+  max,
+  gt
+} from "drizzle-orm";
 
 async function main() {
 
@@ -24,130 +32,130 @@ async function main() {
   /* ============================
      UPDATE
      ============================ */
-  await db
-    .update(users)
-    .set({ name: 'Rohit Sharma' })
-    .where(eq(users.id, 1));
+  // await db
+  //   .update(users)
+  //   .set({ name: 'Rohit Sharma' })
+  //   .where(eq(users.id, 1));
 
-  console.log('UPDATE: Name updated for id = 1');
+  // console.log('UPDATE: Name updated for id = 1');
 
-  /* ============================
-     DELETE
-     ============================ */
-  await db
-    .delete(users)
-    .where(eq(users.id, 2));
+  // /* ============================
+  //    DELETE
+  //    ============================ */
+  // await db
+  //   .delete(users)
+  //   .where(eq(users.id, 2));
 
-  console.log('DELETE: Record deleted for id = 2');
+  // console.log('DELETE: Record deleted for id = 2');
 
-  /* ============================
-     WHERE
-     ============================ */
-  const userById = await db
-    .select()
-    .from(users)
-    .where(eq(users.id, 1));
+  // /* ============================
+  //    WHERE
+  //    ============================ */
+  // const userById = await db
+  //   .select()
+  //   .from(users)
+  //   .where(eq(users.id, 1));
 
-  console.log('WHERE id = 1:', userById);
+  // console.log('WHERE id = 1:', userById);
 
-  /* ============================
-     AND
-     ============================ */
-  const andCondition = await db
-    .select()
-    .from(users)
-    .where(
-      and(
-        eq(users.name, 'Rohit'),
-        eq(users.id, 1)
-      )
-    );
+  // /* ============================
+  //    AND
+  //    ============================ */
+  // const andCondition = await db
+  //   .select()
+  //   .from(users)
+  //   .where(
+  //     and(
+  //       eq(users.name, 'Rohit'),
+  //       eq(users.id, 1)
+  //     )
+  //   );
 
-  console.log('AND condition:', andCondition);
+  // console.log('AND condition:', andCondition);
 
-  /* ============================
-     OR
-     ============================ */
-  const orCondition = await db
-    .select()
-    .from(users)
-    .where(
-      or(
-        eq(users.id, 1),
-        eq(users.id, 2)
-      )
-    );
+  // /* ============================
+  //    OR
+  //    ============================ */
+  // const orCondition = await db
+  //   .select()
+  //   .from(users)
+  //   .where(
+  //     or(
+  //       eq(users.id, 1),
+  //       eq(users.id, 2)
+  //     )
+  //   );
 
-  console.log('OR condition:', orCondition);
+  // console.log('OR condition:', orCondition);
 
-  /* ============================
-     LIKE
-     ============================ */
-  const likeQuery = await db
-    .select()
-    .from(users)
-    .where(like(users.name, '%hit%'));
+  // /* ============================
+  //    LIKE
+  //    ============================ */
+  // const likeQuery = await db
+  //   .select()
+  //   .from(users)
+  //   .where(like(users.name, '%hit%'));
 
-  console.log('LIKE query:', likeQuery);
+  // console.log('LIKE query:', likeQuery);
 
-  /* ============================
-     IN
-     ============================ */
-  const inQuery = await db
-    .select()
-    .from(users)
-    .where(inArray(users.id, [1, 2, 3]));
+  // /* ============================
+  //    IN
+  //    ============================ */
+  // const inQuery = await db
+  //   .select()
+  //   .from(users)
+  //   .where(inArray(users.id, [1, 2, 3]));
 
-  console.log('IN query:', inQuery);
+  // console.log('IN query:', inQuery);
 
-  /* ============================
-     BETWEEN
-     ============================ */
-  const betweenQuery = await db
-    .select()
-    .from(users)
-    .where(between(users.id, 1, 5));
+  // /* ============================
+  //    BETWEEN
+  //    ============================ */
+  // const betweenQuery = await db
+  //   .select()
+  //   .from(users)
+  //   .where(between(users.id, 1, 5));
 
-  console.log('BETWEEN query:', betweenQuery);
+  // console.log('BETWEEN query:', betweenQuery);
 
-  /* ============================
-     ORDER BY ASC
-     ============================ */
-  const orderAsc = await db
-    .select()
-    .from(users)
-    .orderBy(asc(users.id));
+  // /* ============================
+  //    ORDER BY ASC
+  //    ============================ */
+  // const orderAsc = await db
+  //   .select()
+  //   .from(users)
+  //   .orderBy(asc(users.id));
 
-  console.log('ORDER BY ASC:', orderAsc);
+  // console.log('ORDER BY ASC:', orderAsc);
 
-  /* ============================
-     ORDER BY DESC
-     ============================ */
-  const orderDesc = await db
-    .select()
-    .from(users)
-    .orderBy(desc(users.id));
+  // /* ============================
+  //    ORDER BY DESC
+  //    ============================ */
+  // const orderDesc = await db
+  //   .select()
+  //   .from(users)
+  //   .orderBy(desc(users.id));
 
-  console.log('ORDER BY DESC:', orderDesc);
+  // console.log('ORDER BY DESC:', orderDesc);
 
-  /* ============================
-     LIMIT
-     ============================ */
-  const limitedUsers = await db
-    .select()
-    .from(users)
-    .limit(2);
+  // /* ============================
+  //    LIMIT
+  //    ============================ */
+  // const limitedUsers = await db
+  //   .select()
+  //   .from(users)
+  //   .limit(2);
 
-  console.log('LIMIT 2:', limitedUsers);
+  // console.log('LIMIT 2:', limitedUsers);
 
-  /* ============================
-     DISTINCT
-     ============================ */
-  const distinctEmails = await db
-    .selectDistinct({ email: users.email })
-    .from(users);
+  // /* ============================
+  //    DISTINCT
+  //    ============================ */
+  // const distinctEmails = await db
+  //   .selectDistinct({ email: users.email })
+  //   .from(users);
 
-  console.log('DISTINCT emails:', distinctEmails);
+  // console.log('DISTINCT emails:', distinctEmails);
 
   // /* ============================
   //    INNER JOIN (Users + Posts)
@@ -188,8 +196,72 @@ async function main() {
 
   // console.log('RIGHT JOIN:', rightJoinData);
 
+   /** SUM  */
 
-  
+const totalUsers = await db
+  .select({ total: count() })
+  .from(users);
+
+console.log("COUNT (total users):", totalUsers);
+
+ /** AVG  */  
+
+ const avgUserId = await db
+  .select({ averageId: avg(users.id)})
+  .from(users);
+
+console.log("AVG (average user id", avgUserId);
+
+/** MIN/MAX */
+
+ const minmaxUserId = await db
+   .select({
+    minAge: min(users.id),
+    maxAge: max(users.id),
+  })
+   .from(users);
+
+ console.log("MIN/MAX (minimum/maximum user id", minmaxUserId);
+
+/** GROUP BY Name */ 
+
+const groupByName = await db
+  .select({
+    name: users.name,
+    totalUsers: count(),
+  })
+  .from(users)
+  .groupBy(users.name);
+
+console.log("GROUP BY name:", groupByName);   
+
+/** HAVING */
+
+const havingData = await db
+  .select({
+    name: users.name,
+    totalUsers: count(),
+  })
+  .from(users)
+  .groupBy(users.name)
+  .having(gt(count(), 1));
+
+console.log("HAVING:", havingData);
+
+/** FINAL COMBINED */
+
+const finalQuery = await db
+  .select({
+    name: users.name,
+    avgUserId: avg(users.id),
+  })
+  .from(users)
+  .where(gt(users.id, 18))
+  .groupBy(users.name)
+  .having(gt(avg(users.id), 25));
+
+console.log("FINAL QUERY:", finalQuery);
+
 }
 
 main();
